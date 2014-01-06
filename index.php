42a9302ace952eddced99b2b04b4e5eab3c70f64
Heikki Pesonen <heikki.pesonen@luotta.fi>
2014-01-07 01:25:22 +0200

    progress

--- index.php
+++ index.php
@@ -4,6 +4,40 @@ include('collector.php');
 addCss('css');
 addScript('src');
 addScript('js');
-
-include('main.html');
 ?>
+
+<html>
+	<head>
+		<meta charset=utf-8>
+		<title>Heikki Pesonen - portfolio</title>
+	</head>
+	<body>
+		<div id="wrapper">
+
+
+			<div id="indicator">
+				<div data-target="left" class="item sliderButton"></div>
+				<div data-target="center" class="item sliderButton"></div>
+				<div data-target="right" class="item sliderButton"></div>
+			</div>
+
+
+			<div id="slider" class="right">
+
+	
+
+<?php
+	include('cv.html');
+	include('main.html');
+	include('portfolio.html');
+
+?>
+
+			</div>
+			<div id="overlay" class="hidden"></div>
+			<div id="fullviewer" class="hidden">
+
+			</div>
+		</div>
+	</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 $(function(){
 	/*
 
-			this is by no means the show-off of my skillz as a web developer..
-			this just was the easiest way to quickly implement new "carousel" to show content...
+
+		hmh...
+
+
+			just the fastest way to create this layout....... nothing fancy here.
 	*/
 
-	var slider = $('#slider');
-	var indicator = $('#indicator');		
-	var cvScroll = new IScroll('#cv-scroll');
+	var iscrollOptions = {
+		mouseWheel:true,
+		scrollbars:true,
+		hideScrollbar:true,
+		
+	}
 
+	var slider = $('#slider'),
+		indicator = $('#indicator'),
+		cvScroll = new IScroll('#cv-scroll',iscrollOptions),
+		pfScroll = new IScroll('#pf-scroll',iscrollOptions),
+		fullViewScroll = false;
+
+	
+		// starting position for bottom page indicator circles..
 	indicator.find('[data-target="center"]').addClass('selected');
 
+	// all .sliderButtons should work the same way... so...
 	$('#wrapper').on('click','.sliderButton',function(){
+		// data-target attribute gives the class name for the slider to use (positions defined in css)
 		showPage($(this).attr('data-target'));
 	});
 
@@ -23,7 +39,10 @@ $(function(){
 		startEvent = e;
 	});
 
+
 	$('#wrapper').on('mouseup touchend',function(e){
+		// when enough time has passed and distance is over set limit, then we can assume the swipe
+		// event has occurred and start thinking about the appropriate action.
 		if (e.timeStamp - startEvent.timeStamp > 100 && e.timeStamp - startEvent.timeStamp < 500){
 			var diff = e.pageX - startEvent.pageX;
 
@@ -46,10 +65,45 @@ $(function(){
 	});
 
 
+	// this just actually adds a class to the slider element, positions defined in css
+	// so it will animate itself into the position with css transforms...
 	function showPage(name){
+		// clear all page classes from the slider
 		slider.removeClass('center').removeClass('left').removeClass('right');
-		slider.addClass(name);
+		slider.addClass(name); // show the new page
+
+		// setup the indicators
 		indicator.find('.selected').removeClass('selected');
 		indicator.find('[data-target="'+name+'"]').addClass('selected');		
+	}
+
+
+	$('ul#list').on('click','li',function(){
+		if ($(this).find('.fullview').length > 0){
+			showFull( $(this).find('.fullview') );
+		}
+	});
+
+
+
+
+	var fullviewer = $('#fullviewer'),
+		overlay = $('#overlay');
+
+	overlay.on('click',hideViewer);
+	fullviewer.on('click','.close',hideViewer);
+
+	function hideViewer(){		
+		overlay.addClass('hidden');
+		fullviewer.addClass('hidden');
+		fullViewScroll.destroy();
+	}
+
+	function showFull(item){
+		fullviewer.html(item.clone());	
+		fullviewer.removeClass('hidden');
+		overlay.removeClass('hidden');
+		
+		fullViewScroll = new IScroll('#fullviewer',iscrollOptions);
 	}
 });
